fbf5b54d97e11a70b79e75c833618a951bea677f
Abraham Amani Ndonga <amaniabraham0@gmail.com>
2020-02-11 01:47:46 -0500

    fixed the clear function issue

--- src/components/card.js
+++ src/components/card.js
@@ -8,19 +8,21 @@ class Card extends Component {
     super();
 
     this.state = {
-      color: "",
-      pluralNoun: "",
-      adjectiveOne: "",
-      celebOne: "",
-      adjectiveTwo: "",
-      nouneOne: "",
-      nounTwo: "",
-      adjectiveThree: "",
-      celebTwo: "",
-      celebThree: "",
-      adjectiveFour: "",
-      nounThree: "",
-      celebFour: "",
+      color: '',
+            pluralNoun: '',
+            adjectiveOne: '',
+            celebOne: '',
+            adjectiveTwo: '',
+            nounOne: '',
+            numberOne: '',
+            numberTwo: '',
+            nounTwo: '',
+            adjectiveThree: '',
+            celebTwo: '',
+            celebThree: '',
+            adjectiveFour: '',
+            nounThree: '',
+            celebFour: '',
       adjectiveFive: "",
       contentVisible: false
     };
@@ -35,49 +37,53 @@ class Card extends Component {
 
   handleFormSubmit(event) {
       event.preventDefault();
-      this.setState({ contentVisible: !this.state.contentVisible })
+
+      if(this.state.contentVisible) {
+        this.setState({
+          color: '',
+            pluralNoun: '',
+            adjectiveOne: '',
+            celebOne: '',
+            adjectiveTwo: '',
+            nounOne: '',
+            numberOne: '',
+            numberTwo: '',
+            nounTwo: '',
+            adjectiveThree: '',
+            celebTwo: '',
+            celebThree: '',
+            adjectiveFour: '',
+            nounThree: '',
+            celebFour: '',
+          adjectiveFive: "",
+          contentVisible: false
+        })
+      } else {
+        this.setState({ contentVisible: true })
+      }
+
   }
   render() {
     const inputData = [
-      { title: "Color", state: this.state.color, name: "color" },
-      {
-        title: "Plural Noun",
-        state: this.state.pluralNoun,
-        name: "pluralNoun"
-      },
-      { title: "Adjective", state: this.state.adjective, name: "adjectiveOne" },
-      { title: "Celebrity", state: this.state.celebOne, name: "celebOne" },
+      {title: 'Color', state: this.state.color, name: 'color'},
+      {title: 'Plural Noun', state: this.state.pluralNoun, name: 'pluralNoun'},
+      {title: 'Adjective', state: this.state.adjectiveOne, name: 'adjectiveOne'},
+      {title: 'Celebrity', state: this.state.celebOne, name: 'celebOne'},
 
-      {
-        title: "Adjective",
-        state: this.state.adjectiveTwo,
-        name: "adjectiveTwo"
-      },
-      { title: "Noun", state: this.state.nounOne, name: "nounOne" },
-      { title: "Number", state: this.state.numberOne, name: "numberOne" },
-      { title: "Number", state: this.state.numberTwo, name: "numberTwo" },
+      {title: 'Adjective', state: this.state.adjectiveTwo, name: 'adjectiveTwo'},
+      {title: 'Noun', state: this.state.nounOne, name: 'nounOne'},
+      {title: 'Number', state: this.state.numberOne, name: 'numberOne'},
+      {title: 'Number', state: this.state.numberTwo, name: 'numberTwo'},
 
-      { title: "Noun", state: this.state.nounTwo, name: "nounTwo" },
-      {
-        title: "Adjective",
-        state: this.state.adjectiveThree,
-        name: "adjectiveThree"
-      },
-      { title: "Celebrity", state: this.state.celebTwo, name: "celebTwo" },
-      { title: "Celebrity", state: this.state.celebThree, name: "celebThree" },
+      {title: 'Noun', state: this.state.nounTwo, name: 'nounTwo'},
+      {title: 'Adjective', state: this.state.adjectiveThree, name: 'adjectiveThree'},
+      {title: 'Celebrity', state: this.state.celebTwo, name: 'celebTwo'},
+      {title: 'Celebrity', state: this.state.celebThree, name: 'celebThree'},
 
-      {
-        title: "Adjective",
-        state: this.state.adjectiveFour,
-        name: "adjectiveFour"
-      },
-      { title: "Noun", state: this.state.nounThree, name: "nounThree" },
-      { title: "Celebrity", state: this.state.celebFour, name: "celebFour" },
-      {
-        title: "Adjective",
-        state: this.state.adjectiveFive,
-        name: "adjectiveFive"
-      }
+      {title: 'Adjective', state: this.state.adjectiveFour, name: 'adjectiveFour'},
+      {title: 'Noun', state: this.state.nounThree, name: 'nounThree'},
+      {title: 'Celebrity', state: this.state.celebFour, name: 'celebFour'},
+      {title: 'Adjective', state: this.state.adjectiveFive, name: 'adjectiveFive'},
     ];
     return (
       <form  onSubmit={this.handleFormSubmit} className="card">
